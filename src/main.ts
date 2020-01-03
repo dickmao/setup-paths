c9c7c5a4b471714b166d6b7271d40f8177377745
dickmao <none>
2020-01-03 17:29:38 -0500

    initial

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,11 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`)
+    const paths: string = core.getInput('paths');
+    const PATH = process.env.PATH;
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    core.setOutput('time', new Date().toTimeString())
+    core.exportVariable("PATH", `${PATH}:${paths}`);
   } catch (error) {
     core.setFailed(error.message)
   }
